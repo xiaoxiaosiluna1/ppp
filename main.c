cefff2cceac2a67ed14c856a18c32909314044c2
siluna <2261906492@qq.com>
2019-01-15 16:45:22 +0800

    项目初始化

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int qian=0;
+
+    printf("jinruqianbaoguanlixitong");
+    while(1){
+        printf("1---cunqian\n");
+        printf("2---quqian\n");
+        printf("3---yue\n");
+        printf("4---tuichu\n");
+        printf("qingxuanzexiangyinggongneng");
+        int code;
+        scanf("%d",&code);
+        if(code == 1){
+            printf("jinrunqiangongneng,qingshuruyaocunrudejine:\n");
+            int cun;
+            scanf("%d",&cun);
+            qian +=cun;
+            printf("cunruchenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+       
+        }
+
+   
+
+    return 0;
+
+}
